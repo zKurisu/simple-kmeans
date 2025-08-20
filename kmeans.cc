5ac591726432b009961dfa67e0092ab8fd264f81
Jie Orkarin <2694783083@qq.com>
2025-08-20 13:55:27 +0800

    Basic structure of kmeans

---
+++ kmeans.cc
@@ -0,0 +1,14 @@
+#include "kmeans.h"
+
+namespace kmeans {
+
+    // Random init points
+    void Kmeans::initPoints(int pointNum) {
+
+    }
+    // Random init points
+    void Kmeans::initPoints(std::vector<Point> pointVec) {
+
+    }
+
+}
---
+++ kmeans.h
@@ -0,0 +1,43 @@
+#ifndef MY_KMEANS_H_
+#define MY_KMEANS_H_
+
+#include <vector>
+#include <map>
+#include <cmath>
+#include <string>
+
+namespace kmeans {
+
+struct Point {
+    int _x; int _y;
+    int group = 0;
+    Point(int x, int y) { _x = x; _y = y; };
+    inline float distance(Point& another) {
+        return (std::sqrtf(_x - another._x) * (_x - another._x) + (_y - another._y) * (_y - another._y));
+    };
+};
+
+
+// Class Definition
+class Kmeans {
+public:
+    Kmeans(int K) { k = K; };
+    void initPoints(int pointNum);
+    void initPoints(std::vector<Point> pointVec);
+    void initCentersTraditional();
+    void initCentersPlusPlus();
+    void update(); // Loop to end
+    void saveResult();
+
+private:
+    int k;
+    std::vector<Point> points;
+    std::map<int,Point> centers; // gid -> Point
+};
+// End Class Definition
+
+std::vector<Point> readFilePoints(std::string fileName);
+
+}
+
+#endif // MY_KMEANS_H_
